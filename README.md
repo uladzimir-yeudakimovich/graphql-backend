2dbac02cf05d5ff70db4942110e1f419ec51c2ae
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-06-23 16:28:44 +0300

    start server

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-## GraphQL
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:4000](http://localhost:4000) to view it in the browser.
---
+++ schema.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server');
+let persons = require('./persons');
+
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String! 
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`;
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) =>
+      persons.find(p => p.name === args.name)
+  },
+  Person: {
+    name: (root) => root.name,
+    phone: (root) => root.phone,
+    street: (root) => root.street,
+    city: (root) => root.city,
+    id: (root) => root.id
+  }
+};
+
+module.exports = { typeDefs, resolvers };
