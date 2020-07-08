c76ca39de782adf5d098e2c38c2f19f0a5f8d2f2
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2020-07-09 00:20:26 +0300

    check token is ready

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET_KEY } = require('./common/config');
 const connectToDb = require('./db/db');
 const { typeDefs, resolvers } = require('./schema');
+const User = require('./models/user');
 
 const server = new ApolloServer({
   typeDefs,
@@ -13,9 +14,8 @@ const server = new ApolloServer({
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decodedToken = jwt.verify( auth.substring(7), JWT_SECRET_KEY, async (err, decoded) => {
         if (err) return;
-        // const currentUser = await User.findById(decodedToken.id).populate('friends');
-        const currentUser = await User.findById(decoded.id).populate('favoriteGenre');
-        console.log(currentUser);
+        // const currentUser = await User.findById(decoded.id).populate('friends');
+        const currentUser = await User.findById(decoded.id);
         return { currentUser };
       });
     }
